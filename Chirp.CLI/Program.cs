550ed4ad9c2216e7f3da4c1243c14f9d1bf93817
CptShini <gabrielnoah.fabricius@gmail.com>
2024-09-11 12:45:17 +0200

    Variable naming and spelling

--- Chirp.CLI/Program.cs
+++ Chirp.CLI/Program.cs
@@ -4,22 +4,22 @@ namespace Chirp.CLI
 {
     internal class Program
     {
-        const string cheepCsvPath = "Chirp.CLI/data/chirp_cli_db.csv";
+        const string CheepCsvPath = "Chirp.CLI/data/chirp_cli_db.csv";
         static async Task<int> Main(string[] args)
         {
-            UserInterface.setCheepsCsvPath(cheepCsvPath);
+            UserInterface.setCheepsCsvPath(CheepCsvPath);
             var rootCommand = new RootCommand("Chirp where you can send cheeps and read others");
 
-            var readCommand = new Command("read", "Read informantion stored in database");
+            var readCommand = new Command("read", "Read information stored in database");
             readCommand.SetHandler(async () =>
             {
                 await UserInterface.ReadCheeps();
             });
 
-            var storeCommand = new Command("cheep", "Add a cheep to the databas");
+            var storeCommand = new Command("cheep", "Add a cheep to the database");
             var cheepArgument = new Argument<string>("message", "The cheep you want to send");
             storeCommand.AddArgument(cheepArgument);
-            storeCommand.SetHandler(async (string message) =>
+            storeCommand.SetHandler(async message =>
              {
                  await UserInterface.WriteCheep(message);   
              }, cheepArgument);
