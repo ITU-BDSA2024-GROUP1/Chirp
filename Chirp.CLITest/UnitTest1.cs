d1eeb7eb0b535552572b66ebf46e3f0f5b660436
= <embs@itu.dk>
2024-09-11 15:01:52 +0200

    Made the test folder and projects and ensured they are part of the solution

---
+++ Chirp.CLITest/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace Chirp.CLITest
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ SimpleDBTest/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace SimpleDBTest
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
