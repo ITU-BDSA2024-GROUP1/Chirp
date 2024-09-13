c592b0ab374f040b67bd49ab602659cbeab1e280
Akn <adni@itu.dk>
2024-09-13 15:56:13 +0200

    Tests for Cheep.cs completed

--- test/Chirp.CLITest/UnitTests.cs
+++ test/Chirp.CLITest/UnitTests.cs
@@ -7,7 +7,7 @@ namespace Chirp.CLITest
     public class UnitTests
     {
         [Fact]
-        public void Cheep_Instantiation_ShouldCreateObject()
+        public void Cheep_Instantiation()
         {
             // Arrange
             string author = "Author";
@@ -24,7 +24,7 @@ namespace Chirp.CLITest
         }
 
         [Fact]
-        public void Cheep_ToString_ShouldFormatCorrectly()
+        public void Cheep_ToString()
         {
             // Arrange
             string author = "Author";
@@ -32,6 +32,22 @@ namespace Chirp.CLITest
             long timestamp = 1627846261;
             var cheep = new Cheep(author, message, timestamp);
             string expected = "Author          @ 08-01-21 21:31:01: Message";
+
+            // Act
+            string result = cheep.ToString();
+
+            // Assert
+            Assert.Equal(expected, result);
+        }
+
+        [Theory]
+        [InlineData("Author", "Message", 0, "Author          @ 01-01-70 01:00:00: Message")] // Unix epoch start
+        [InlineData("Author", "Message", 253402300799, "Author          @ 12-31-99 23:59:59: Message")] // End of 9999 year
+        [InlineData("Author", "Message", 10000000000, "Author          @ 11-20-86 18:46:40: Message")] // Far future date
+        public void Cheep_ToString_ExtremeTimestamps(string author, string message, long timestamp, string expected)
+        {
+            // Arrange
+            var cheep = new Cheep(author, message, timestamp);
 
             // Act
             string result = cheep.ToString();
