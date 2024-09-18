e56e3e715da9b231fb377f0bad33417d7a04ff62
Noah <gafa@itu.dk>
2024-09-18 16:49:31 +0200

    Removed unnecessary access modifiers in interface.

--- src/SimpleDB/IDatabaseRepository.cs
+++ src/SimpleDB/IDatabaseRepository.cs
@@ -2,6 +2,6 @@ namespace SimpleDB;
 
 public interface IDatabaseRepository<T>
 {
-    public IEnumerable<T> Read(int? limit = null);
-    public void Store(T record);
+    IEnumerable<T> Read(int? limit = null);
+    void Store(T record);
 }
