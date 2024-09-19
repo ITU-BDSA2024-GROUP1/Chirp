eb1e57d2e3e7bce62ef1873e4b20dcb6593b0818
CptShini <gabrielnoah.fabricius@gmail.com>
2024-09-19 10:40:43 +0200

    Internals no longer needed to be visible

--- src/Chirp.CLI/Properties/AssemblyInfo.cs
+++ src/Chirp.CLI/Properties/AssemblyInfo.cs
@@ -1,6 +1,4 @@
 ﻿using System.Runtime.CompilerServices;
 
 [assembly: InternalsVisibleTo("Chirp.CLITest")]
-[assembly: InternalsVisibleTo("Chirp.CSVDBService")]
-[assembly: InternalsVisibleTo("SimpleDBTest")]
 [assembly: InternalsVisibleTo("DynamicProxyGenAssembly2")]
