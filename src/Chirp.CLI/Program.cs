b93307dce0f66fafbba5ca56582cb0b3d6df09f6
CptShini <gabrielnoah.fabricius@gmail.com>
2024-09-19 12:26:24 +0200

    Moved to file-scoped namespaces.

--- src/Chirp.CLI/Program.cs
+++ src/Chirp.CLI/Program.cs
@@ -4,29 +4,28 @@ using Chirp.Core;
 
 using SimpleDB;
 
-namespace Chirp.CLI
+namespace Chirp.CLI;
+
+internal class Program
 {
-    internal class Program
+    static async Task<int> Main(string[] args)
     {
-        static async Task<int> Main(string[] args)
-        {
-            DirectoryFixer.SetWorkingDirectoryToProjectRoot();
-            var rootCommand = new RootCommand("Chirp where you can send cheeps and read others");
+        DirectoryFixer.SetWorkingDirectoryToProjectRoot();
+        var rootCommand = new RootCommand("Chirp where you can send cheeps and read others");
             
-            var readCommand = new Command("read", "Read information stored in database");
-            var readArgument = new Argument<int?>("value", "The amount of latest cheeps you want to read");
-            readCommand.AddArgument(readArgument);
-            readCommand.SetHandler(UserInterface.ReadCheeps, readArgument);
+        var readCommand = new Command("read", "Read information stored in database");
+        var readArgument = new Argument<int?>("value", "The amount of latest cheeps you want to read");
+        readCommand.AddArgument(readArgument);
+        readCommand.SetHandler(UserInterface.ReadCheeps, readArgument);
 
-            var storeCommand = new Command("cheep", "Add a cheep to the database");
-            var cheepArgument = new Argument<string>("message", "The cheep you want to send");
-            storeCommand.AddArgument(cheepArgument);
-            storeCommand.SetHandler(UserInterface.WriteCheep, cheepArgument);
+        var storeCommand = new Command("cheep", "Add a cheep to the database");
+        var cheepArgument = new Argument<string>("message", "The cheep you want to send");
+        storeCommand.AddArgument(cheepArgument);
+        storeCommand.SetHandler(UserInterface.WriteCheep, cheepArgument);
 
-            rootCommand.AddCommand(readCommand);
-            rootCommand.AddCommand(storeCommand);
+        rootCommand.AddCommand(readCommand);
+        rootCommand.AddCommand(storeCommand);
 
-            return await rootCommand.InvokeAsync(args);
-        }
+        return await rootCommand.InvokeAsync(args);
     }
 }
--- src/SimpleDB/CSVDatabase.cs
+++ src/SimpleDB/CSVDatabase.cs
@@ -3,45 +3,45 @@
 using CsvHelper;
 using CsvHelper.Configuration;
 
-namespace SimpleDB
+namespace SimpleDB;
+
+public class CSVDatabase<T> : IDatabaseRepository<T>
 {
-    public class CSVDatabase<T> : IDatabaseRepository<T>
+    private const string Path = "data/chirp_cli_db.csv";
+    private const string TestPath = "data/test.csv";
+
+    public bool InTestingDatabase = false;
+    private string CurrentDBPath => InTestingDatabase ? TestPath : Path;
+
+    private static CSVDatabase<T>? s_instance;
+    private CSVDatabase() {}
+    public static CSVDatabase<T> Instance
     {
-        private const string Path = "data/chirp_cli_db.csv";
-        private const string TestPath = "data/test.csv";
+        get => s_instance ??= new CSVDatabase<T>();
+    }
 
-        public static bool InTestingDatabase = false;
-
-        private static CSVDatabase<T>? s_instance;
-        private CSVDatabase() {}
-        public static CSVDatabase<T> Instance
-        {
-            get => s_instance ??= new CSVDatabase<T>();
-        }
-
-        public IEnumerable<T> Read(int? limit = null)
-        {
-            using StreamReader reader = new(InTestingDatabase ? TestPath : Path);
-            using CsvReader csv = new(reader, CultureInfo.InvariantCulture);
+    public IEnumerable<T> Read(int? limit = null)
+    {
+        using StreamReader reader = new(CurrentDBPath);
+        using CsvReader csv = new(reader, CultureInfo.InvariantCulture);
             
-            IEnumerable<T> records = csv.GetRecords<T>().ToList();
-            return limit == null ? records : records.TakeLast((int)limit).ToList();
-        }
+        IEnumerable<T> records = csv.GetRecords<T>().ToList();
+        return limit == null ? records : records.TakeLast((int)limit).ToList();
+    }
 
-        public void Store(T record)
+    public void Store(T record)
+    {
+        CsvConfiguration config = new(CultureInfo.InvariantCulture)
         {
-            CsvConfiguration config = new(CultureInfo.InvariantCulture)
-            {
-                // Don't write the header again.
-                HasHeaderRecord = false
-            };
+            // Don't write the header again.
+            HasHeaderRecord = false
+        };
 
-            using FileStream stream = File.Open(InTestingDatabase ? TestPath : Path, FileMode.Append);
-            using StreamWriter writer = new(stream);
-            using CsvWriter csv = new(writer, config);
+        using FileStream stream = File.Open(CurrentDBPath, FileMode.Append);
+        using StreamWriter writer = new(stream);
+        using CsvWriter csv = new(writer, config);
             
-            csv.WriteRecord(record);
-            csv.NextRecord();
-        }
+        csv.WriteRecord(record);
+        csv.NextRecord();
     }
 }
