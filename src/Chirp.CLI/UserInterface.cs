7d62f9c48fb7b046b52e02a65a16c0c60c4e64d5
CptShini <gabrielnoah.fabricius@gmail.com>
2024-09-19 10:38:49 +0200

    Refactored CLI's UserInterface class to use HTTP requests.
    Made a new WebDB class that interacts with the web service on localhost. It isn't perfect and I want to go back to the dependency injection I was using before, but that requires some more work.

--- src/Chirp.CLI/UserInterface.cs
+++ src/Chirp.CLI/UserInterface.cs
@@ -1,21 +1,17 @@
 ﻿using Chirp.Core;
 
-using SimpleDB;
-
 namespace Chirp.CLI;
 
 internal static class UserInterface
 {
-    private static readonly DBCommunicator Communicator = new DBCommunicator(CSVDatabase<Cheep>.Instance);
-    
     public static void ReadCheeps(int? limit = null)
     {
-        var cheeps = Communicator.ReadCheeps(limit);
+        var cheeps = WebDB.ReadCheeps(limit);
         foreach (Cheep cheep in cheeps)
         {
             Console.WriteLine(cheep);
         }
     }
 
-    public static void WriteCheep(string message) => Communicator.WriteCheep(message);
+    public static void WriteCheep(string message) => WebDB.WriteCheep(message);
 }
---
+++ src/Chirp.CLI/WebDB.cs
@@ -0,0 +1,36 @@
+﻿using System.Net.Http.Json;
+
+using Chirp.Core;
+
+using SimpleDB;
+
+namespace Chirp.CLI;
+
+public static class WebDB
+{
+    private const string BaseUrl = "http://localhost:5127";
+    private static readonly HttpClient Client = new()
+    {
+        BaseAddress = new Uri(BaseUrl),
+        DefaultRequestHeaders =
+        {
+            Accept =
+            {
+                new("application/json")
+            }
+        }
+    };
+    
+    public static IEnumerable<Cheep> ReadCheeps(int? limit = null)
+    {
+        string endpoint = "cheeps" + (limit == null ? string.Empty : $"?limit={limit}");
+        var cheeps = Client.GetFromJsonAsync<IEnumerable<Cheep>>(endpoint);
+        return cheeps.Result!;
+    }
+
+    public static void WriteCheep(string message)
+    {
+        string endpoint = $"cheep?message={message}";
+        Client.PostAsJsonAsync(endpoint, "how does this work").Wait();
+    }
+}
