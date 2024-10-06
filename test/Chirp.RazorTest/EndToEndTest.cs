f5e7625a75ad51c39d8c0000b30b6efe4151c02c
= <embs@itu.dk>
2024-10-06 23:45:47 +0200

    Added endtoend test class

---
+++ test/Chirp.RazorTest/EndToEndTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chirp.RazorTest
+{
+    public class EndToEndTest
+    {
+        [Fact]
+        public void Code200()
+        {
+            // Arrange
+
+            // Act
+
+            
+            // Assert
+
+        }
+    }
+}
