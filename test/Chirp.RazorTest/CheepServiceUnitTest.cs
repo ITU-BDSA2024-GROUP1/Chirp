14ffb63fd87c3b0045258ff490c1bb8b09f4ded7
Emil <embs@itu.dk>
2024-10-09 12:56:12 +0200

    Fixed tests breaking from Pagination
    Co-authored-by: Adam <adni@itu.dk>
    Co-authored-by: Mathias <matal@itu.dk>
    Co-authored-by: Niels <nbay@itu.dk>

--- test/Chirp.RazorTest/CheepServiceUnitTest.cs
+++ test/Chirp.RazorTest/CheepServiceUnitTest.cs
@@ -35,10 +35,10 @@ public class CheepServiceUnitTest
         string dbPath = Environment.GetEnvironmentVariable("CHIRPDBPATH") ?? System.IO.Path.Combine(System.IO.Path.GetTempPath(), "chirp.db");
         DBFacade dbFacade = new DBFacade(dbPath);
         CheepService cheepService = new CheepService(dbFacade);
-        CheepViewModel expected = new CheepViewModel("Jacqualine Gilcoine", "They were married in Chicago, with old Smith, and was expected aboard every day; meantime, the two went past me.", 1690895677);
+        CheepViewModel expected = new CheepViewModel("Jacqualine Gilcoine", "Starbuck now is what we hear the worst.", 1690895859);
 
         // Act
-        List<CheepViewModel> test = cheepService.GetCheeps();
+        List<CheepViewModel> test = cheepService.GetCheeps(0);
 
         // Assert
         Assert.Equal(expected, test[0]);
@@ -52,11 +52,11 @@ public class CheepServiceUnitTest
         string dbPath = Environment.GetEnvironmentVariable("CHIRPDBPATH") ?? System.IO.Path.Combine(System.IO.Path.GetTempPath(), "chirp.db");
         DBFacade dbFacade = new DBFacade(dbPath);
         CheepService cheepService = new CheepService(dbFacade);
-        CheepViewModel notExpected = new CheepViewModel("Jacqualine Gilcoine", "They were married in Chicago, with old Smith, and was expected aboard every day; meantime, the two went past me.", 1690895677);
-        CheepViewModel expected = new CheepViewModel("Quintin Sitts", "Unless we succeed in establishing ourselves in some monomaniac way whatever significance might lurk in them.", 1690895674);
+        CheepViewModel notExpected = new CheepViewModel("Jacqualine Gilcoine", "Starbuck now is what we hear the worst.", 1690895859);
+        CheepViewModel expected = new CheepViewModel("Quintin Sitts", "It's bad enough to appal the stoutest man who was my benefactor, and all for our investigation.", 1690895852);
 
         // Act
-        List<CheepViewModel> test = cheepService.GetCheepsFromAuthor("Quintin Sitts");
+        List<CheepViewModel> test = cheepService.GetCheepsFromAuthor("Quintin Sitts", 0);
 
         // Assert
         Assert.Equal(expected, test[0]);
