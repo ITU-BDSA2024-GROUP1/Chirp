ea0bc51850b7f21c83be715176ed9018079a200d
= <embs@itu.dk>
2024-10-27 14:25:31 +0100

    #45 Moved the author service stuff to its own class for proper delegation
    Co-authored-by: Adam <adni@itu.dk>
    Co-authored-by: Niels <nbay@itu.dk>
    Co-authored-by: Noah <gafa@itu.dk>

---
+++ src/Chirp.Infrastructure/AuthorService.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Chirp.Core.Models;
+using Chirp.Core.Repositories;
+
+namespace Chirp.Infrastructure;
+
+public record AuthorViewModel(int Id, string Name, string Email);
+public interface IAuthorService
+{
+    public Task<AuthorViewModel> GetAuthorFromName(string author);
+    public Task<AuthorViewModel> GetAuthorFromEmail(string email);
+}
+public class AuthorService : IAuthorService
+{
+    private readonly IAuthorRepository _authorRepository;
+
+    public AuthorService(IAuthorRepository authorRepository)
+    {
+        _authorRepository = authorRepository;
+    }
+
+    public async Task<AuthorViewModel> GetAuthorFromName(string author)
+    {
+        var authorResult = await _authorRepository.GetAuthorByNameAsync(author);
+        return new AuthorViewModel(authorResult.Id, authorResult.Name, authorResult.Email);
+    }
+
+    public async Task<AuthorViewModel> GetAuthorFromEmail(string email)
+    {
+        var authorResult = await _authorRepository.GetAuthorByEmailAsync(email);
+        return new AuthorViewModel(authorResult.Id, authorResult.Name, authorResult.Email);
+    }
+}
