d0ab0af9908b6d2ed41642d438df5161b877d168
CptShini <gabrielnoah.fabricius@gmail.com>
2024-10-31 12:49:43 +0100

    Refactored/Simplified the infrastructure unit tests.
    Pretty much gave it the exact same treatment as the core unit tests.

--- test/Chirp.InfrastructureTest/AuthorServiceUnitTest.cs
+++ test/Chirp.InfrastructureTest/AuthorServiceUnitTest.cs
@@ -1,147 +1,72 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Chirp.Core.DataTransferObject;
-using Chirp.Core.Entities;
-using Chirp.Core.Models;
-using Chirp.Core.Repositories;
-
-using Chirp.Core.DataTransferObject;
+﻿using Chirp.Core.DataTransferObject;
 using Chirp.Infrastructure;
 
-namespace Chirp.InfrastructureTest
+namespace Chirp.InfrastructureTest;
+
+public class AuthorServiceUnitTest : InfrastructureServiceTester
 {
-    public class AuthorServiceUnitTest
+    private readonly AuthorService _authorService;
+    
+    public AuthorServiceUnitTest() => _authorService = new(_authorRepository);
+
+    private static AuthorViewModel AuthorDTOToAuthorViewModel(AuthorDTO authorDTO)
     {
+        return new(authorDTO.Id, authorDTO.Name, authorDTO.Email);
+    }
+    
+    [Fact]
+    public void MakeAuthorService()
+    {
+        // Assert
+        Assert.NotNull(_authorService);
+    }
 
-        [Fact]
-        public static async Task MakeAuthorService()
-        {
-            // Arrange
-            using var connection = new SqliteConnection("Data Source=memory");
-            await connection.OpenAsync();
-            var builder = new DbContextOptionsBuilder<ChirpDBContext>().UseSqlite(connection);
+    [Fact]
+    public async Task GetAuthorByName()
+    {
+        // Act
+        AuthorViewModel 
+            expected = AuthorDTOToAuthorViewModel(_knownAuthors[0]), 
+            actual = await _authorService.GetAuthorByName(_knownAuthors[0].Name);
 
-            using var context = new ChirpDBContext(builder.Options);
-            await context.Database.EnsureCreatedAsync();
+        // Assert
+        Assert.Equal(expected, actual);
+    }
 
-            IAuthorRepository authorRepository = new AuthorRepository(context);
-            AuthorDTO[] knownAuthors = await CheepServiceUnitTest.SetUpTestAuthorDB(authorRepository, connection);
+    [Fact]
+    public async Task GetAuthorByEmail()
+    {
+        // Act
+        AuthorViewModel 
+            expected = AuthorDTOToAuthorViewModel(_knownAuthors[2]), 
+            actual = await _authorService.GetAuthorByEmail(_knownAuthors[2].Email);
 
-            // Act
-            IAuthorService authorService = new AuthorService(authorRepository);
+        // Assert
+        Assert.Equal(expected, actual);
+    }
 
-            // Assert
-            Assert.NotNull(authorService);
-        }
+    [Fact]
+    public async Task GetAuthorById()
+    {
+        // Act
+        AuthorViewModel
+            expected = AuthorDTOToAuthorViewModel(_knownAuthors[1]),
+            actual = await _authorService.GetAuthorById(_knownAuthors[1].Id);
 
+        // Assert
+        Assert.Equal(expected, actual);
+    }
 
-            [Fact]
-        public static async Task GetAuthorByName()
-        {
-            // Arrange
-            using var connection = new SqliteConnection("Data Source=memory");
-            await connection.OpenAsync();
-            var builder = new DbContextOptionsBuilder<ChirpDBContext>().UseSqlite(connection);
+    [Fact]
+    public async Task CreateAuthor()
+    {
+        // Act
+        AuthorViewModel expected = new(_knownAuthors[^1].Id + 1, "Test5", "Test5@Tester.com");
+        int authorId = await _authorService.CreateAuthor(expected);
+        
+        AuthorViewModel actual = await _authorService.GetAuthorById(authorId);
 
-            using var context = new ChirpDBContext(builder.Options);
-            await context.Database.EnsureCreatedAsync();
-
-            IAuthorRepository authorRepository = new AuthorRepository(context);
-            AuthorDTO[] knownAuthors = await CheepServiceUnitTest.SetUpTestAuthorDB(authorRepository, connection);
-
-            IAuthorService authorService = new AuthorService(authorRepository);
-
-            // Act
-            AuthorViewModel
-                expected = AuthorDTOToAuthorViewModel(knownAuthors[0]),
-                actual = await authorService.GetAuthorByName(knownAuthors[0].Name);
-
-            // Assert
-            Assert.Equal(expected, actual);
-        }
-
-        [Fact]
-        public static async Task GetAuthorByEmail()
-        {
-            // Arrange
-            using var connection = new SqliteConnection("Data Source=memory");
-            await connection.OpenAsync();
-            var builder = new DbContextOptionsBuilder<ChirpDBContext>().UseSqlite(connection);
-
-            using var context = new ChirpDBContext(builder.Options);
-            await context.Database.EnsureCreatedAsync();
-
-            IAuthorRepository authorRepository = new AuthorRepository(context);
-            AuthorDTO[] knownAuthors = await CheepServiceUnitTest.SetUpTestAuthorDB(authorRepository, connection);
-
-            IAuthorService authorService = new AuthorService(authorRepository);
-
-            // Act
-            AuthorViewModel
-                expected = AuthorDTOToAuthorViewModel(knownAuthors[2]),
-                actual = await authorService.GetAuthorByEmail(knownAuthors[2].Email);
-
-            // Assert
-            Assert.Equal(expected, actual);
-        }
-
-        [Fact]
-        public static async Task GetAuthorById()
-        {
-            // Arrange
-            using var connection = new SqliteConnection("Data Source=memory");
-            await connection.OpenAsync();
-            var builder = new DbContextOptionsBuilder<ChirpDBContext>().UseSqlite(connection);
-
-            using var context = new ChirpDBContext(builder.Options);
-            await context.Database.EnsureCreatedAsync();
-
-            IAuthorRepository authorRepository = new AuthorRepository(context);
-            AuthorDTO[] knownAuthors = await CheepServiceUnitTest.SetUpTestAuthorDB(authorRepository, connection);
-
-            IAuthorService authorService = new AuthorService(authorRepository);
-
-            // Act
-            AuthorViewModel
-                expected = AuthorDTOToAuthorViewModel(knownAuthors[1]),
-                actual = await authorService.GetAuthorById(knownAuthors[1].Id);
-
-            // Assert
-            Assert.Equal(expected, actual);
-        }
-
-        [Fact]
-        public static async Task CreateAuthor()
-        {
-            // Arrange
-            using var connection = new SqliteConnection("Data Source=memory");
-            await connection.OpenAsync();
-            var builder = new DbContextOptionsBuilder<ChirpDBContext>().UseSqlite(connection);
-
-            using var context = new ChirpDBContext(builder.Options);
-            await context.Database.EnsureCreatedAsync();
-
-            IAuthorRepository authorRepository = new AuthorRepository(context);
-            AuthorDTO[] knownAuthors = await CheepServiceUnitTest.SetUpTestAuthorDB(authorRepository, connection);
-
-            IAuthorService authorService = new AuthorService(authorRepository);
-
-            // Act
-            AuthorViewModel expected = new AuthorViewModel(knownAuthors[knownAuthors.Length - 1].Id + 1, "Test5", "Test5@Tester.com");
-            int authorId = await authorService.CreateAuthor(expected);
-            AuthorViewModel actual = await authorService.GetAuthorById(authorId);
-
-            // Assert
-            Assert.Equal(expected, actual);
-        }
-
-        public static AuthorViewModel AuthorDTOToAuthorViewModel(AuthorDTO authorDTO)
-        {
-            return new AuthorViewModel(authorDTO.Id, authorDTO.Name, authorDTO.Email);
-        }
-
+        // Assert
+        Assert.Equal(expected, actual);
     }
 }
---
+++ test/Chirp.InfrastructureTest/InfrastructureServiceTester.cs
@@ -0,0 +1,90 @@
+﻿using Chirp.Core.DataTransferObject;
+using Chirp.Core.Models;
+
+namespace Chirp.InfrastructureTest;
+
+public abstract class InfrastructureServiceTester
+{
+    private readonly SqliteConnection _connection;
+    private protected readonly ChirpDBContext _context;
+    
+    private protected readonly IAuthorRepository _authorRepository;
+    private protected readonly AuthorDTO[] _knownAuthors;
+    
+    private protected InfrastructureServiceTester()
+    {
+        _connection = new("Data Source=memory");
+        _connection.OpenAsync();
+        var builder = new DbContextOptionsBuilder<ChirpDBContext>().UseSqlite(_connection);
+
+        _context = new(builder.Options);
+        _context.Database.EnsureCreatedAsync();
+        
+        _authorRepository = new AuthorRepository(_context);
+        _knownAuthors = SetUpTestAuthorDB(_authorRepository).Result;
+    }
+    
+    private protected async Task ClearDB(string tableName)
+    {
+        await using (var command = new SqliteCommand($"DELETE FROM {tableName};", _connection))
+        {
+            command.ExecuteNonQuery();
+        }
+    }
+    
+    private async Task<AuthorDTO[]> SetUpTestAuthorDB(IAuthorRepository authorRepository)
+    {
+        await ClearDB("authors");
+
+        AuthorDTO[] authors = new AuthorDTO[4];
+        for (int i = 0; i < authors.Length; i++)
+        {
+            int id = i + 1;
+            authors[i] = new()
+            {
+                Id = id,
+                Name = $"Test{id}",
+                Email = $"Test{id}@Tester.com"
+            };
+            authors[i].Id = await authorRepository.AddAuthorAsync(authors[i]);
+        }
+
+        List<AuthorDTO> repoAuthors = [..await authorRepository.GetAllAuthorsAsync()];
+        for (int i = 0; i < authors.Length; i++) authors[i].Id = repoAuthors[i].Id;
+
+        return authors;
+    }
+    
+    private protected async Task<CheepDTO[]> SetUpTestCheepDB(ICheepRepository cheepRepository, AuthorDTO[] authors, int n = 160)
+    {
+        await ClearDB("cheeps");
+
+        DateTime timeStamp = DateTime.Now;
+        CheepDTO[] cheeps = new CheepDTO[n];
+        for (int i = 0; i < cheeps.Length; i++)
+        {
+            timeStamp = timeStamp.AddTicks(10000000);
+            int id = i + 1;
+            int authorIndex = i % authors.Length;
+            cheeps[i] = new()
+            {
+                Id = id,
+                Name = authors[authorIndex].Name,
+                Message = $"Text{id}",
+                TimeStamp = timeStamp.ToString(@"yyyy\-MM\-dd HH\:mm\:ss"),
+                AuthorId = authors[authorIndex].Id,
+                AuthorEmail = authors[authorIndex].Email
+            };
+            cheeps[i].Id = await cheepRepository.AddCheepAsync(cheeps[i]);
+        }
+
+        Array.Reverse(cheeps);
+
+        PagedResult<CheepDTO> pagedCheeps = await cheepRepository.GetAllCheepsAsync(1, cheeps.Length);
+        List<CheepDTO> repoCheeps = pagedCheeps.Items.ToList();
+        for (int i = 0; i < cheeps.Length; i++) cheeps[i].Id = repoCheeps[i].Id;
+
+        return cheeps;
+
+    }
+}
