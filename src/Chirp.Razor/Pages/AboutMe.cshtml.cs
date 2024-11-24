dfc0fe846e6aad8b6373791e31412bbd2acca683
Akn <adni@itu.dk>
2024-11-24 20:08:19 +0100

    Temporary solution for amount of cheeps
    Everything is now displayed except the list of followers, and there will be found a better solution to show the list of cheeps so there isn't being used int.MaxValue for pageSize in the metod.

--- src/Chirp.Razor/Pages/AboutMe.cshtml.cs
+++ src/Chirp.Razor/Pages/AboutMe.cshtml.cs
@@ -1,3 +1,6 @@
+using Azure;
+using System.Drawing.Printing;
+
 using Chirp.Core.Entities;
 using Chirp.Infrastructure.CheepService;
 using Chirp.Razor.Areas.Identity.Pages.Account.Manage;
@@ -12,12 +15,14 @@ namespace Chirp.Razor.Pages
     {
         private readonly SignInManager<Author> _signInManager;
         private readonly UserManager<Author> _userManager;
+        private readonly ICheepService _cheepService;
         public List<CheepViewModel> Cheeps { get; set; } = new List<CheepViewModel>();
 
-        public AboutMeModel(SignInManager<Author> signInManager, UserManager<Author> userManager)
+        public AboutMeModel(SignInManager<Author> signInManager, UserManager<Author> userManager, ICheepService cheepService)
         {
             _signInManager = signInManager;
             _userManager = userManager;
+            _cheepService = cheepService;
         }
 
         [BindProperty]
@@ -32,9 +37,19 @@ namespace Chirp.Razor.Pages
             public string ProviderKey { get; set; }
         }
 
-        public async Task OnGetAsync()
+        public async Task<ActionResult> OnGetAsync()
         {
+            if (!User.Identity.IsAuthenticated)
+            {
+                RedirectToPage("Login"); // Redirect to login page if not authenticated
+            }
+
             var user = await _userManager.FindByNameAsync(User.Identity.Name);
+
+            if (user == null)
+            {
+                RedirectToPage("Public");
+            }
 
             Data = new DataModel()
             {
@@ -51,6 +66,13 @@ namespace Chirp.Razor.Pages
             {
                 githubUser = false;
             }
+
+            var cheepsResult = await _cheepService.GetCheepsFromAuthor(user.UserName, 1, int.MaxValue);
+            if (cheepsResult.Items != null)
+            {
+                Cheeps.AddRange(cheepsResult.Items);
+            }
+            return Page();
         }
     }
 }
