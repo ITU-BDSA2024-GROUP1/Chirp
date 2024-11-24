66e0802be9108445c64ece8048a5ddd428c87cfc
Akn <adni@itu.dk>
2024-11-24 16:16:02 +0100

    Displays: email, username and github info for github user

--- src/Chirp.Razor/Pages/AboutMe.cshtml.cs
+++ src/Chirp.Razor/Pages/AboutMe.cshtml.cs
@@ -1,3 +1,8 @@
+using Chirp.Core.Entities;
+using Chirp.Infrastructure.CheepService;
+using Chirp.Razor.Areas.Identity.Pages.Account.Manage;
+
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -5,8 +10,47 @@ namespace Chirp.Razor.Pages
 {
     public class AboutMeModel : PageModel
     {
-        public void OnGet()
+        private readonly SignInManager<Author> _signInManager;
+        private readonly UserManager<Author> _userManager;
+        public List<CheepViewModel> Cheeps { get; set; } = new List<CheepViewModel>();
+
+        public AboutMeModel(SignInManager<Author> signInManager, UserManager<Author> userManager)
         {
+            _signInManager = signInManager;
+            _userManager = userManager;
+        }
+
+        [BindProperty]
+        public DataModel Data { get; set; }
+
+        public bool githubUser { get; set; }
+
+        public class DataModel
+        {
+            public string Email { get; set; }
+            public string LoginProvider { get; set; }
+            public string ProviderKey { get; set; }
+        }
+
+        public async Task OnGetAsync()
+        {
+            var user = await _userManager.FindByNameAsync(User.Identity.Name);
+
+            Data = new DataModel()
+            {
+                Email = user.Email
+            };
+
+            var info = await _signInManager.GetExternalLoginInfoAsync();
+            if (info != null)
+            {
+                Data.LoginProvider = info.LoginProvider;
+                Data.ProviderKey = info.ProviderKey;
+                githubUser = true;
+            } else
+            {
+                githubUser = false;
+            }
         }
     }
 }
