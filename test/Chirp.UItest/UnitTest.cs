4ec6e10804659217dbe4e1d4417244334e59d3e1
Niels Bay <nielsarthurbay@gmail.com>
2024-11-27 16:02:47 +0100

    #56 setup first test

--- test/Chirp.UItest/UnitTest.cs
+++ test/Chirp.UItest/UnitTest.cs
@@ -1,10 +1,28 @@
-﻿namespace TestProject1;
+﻿using NUnit.Framework.Internal;
 
-public class UnitTest
+namespace TestProject1;
+
+public class UnitTest : PageTest
 {
     [SetUp]
-    public async Task Init()
+    public async Task login()
     {
-        
+        await Page.GotoAsync("https://bdsagroup1chirpremotedb.azurewebsites.net/Identity/Account/Login");
+        await Page.FillAsync("Input.Email", "Adrian");
+        await Page.FillAsync("Input_Password", "M32Want_Access");
+        await Page.ClickAsync("Log in");
+        await Page.WaitForURLAsync("https://bdsagroup1chirpremotedb.azurewebsites.net"); 
     }
+
+    [TearDown]
+    public async Task teardown()
+    {
+        Page.ClickAsync("logout[Adrian]");
+    }
+
+    [Test]
+    public async Task Login()   
+    {
+        await Expect(Page.GetByText("Share")).ToBeVisibleAsync();
+    } 
 }
