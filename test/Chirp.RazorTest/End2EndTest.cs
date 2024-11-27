7043f65067844a59155fd22da6fc8ec69e13b591
CptShini <gabrielnoah.fabricius@gmail.com>
2024-11-27 17:47:36 +0100

    Greatly improved abstraction in regards to designing tests using playwright.

--- test/Chirp.RazorTest/End2EndTest.cs
+++ test/Chirp.RazorTest/End2EndTest.cs
@@ -1,71 +1,37 @@
-using Microsoft.Playwright;
-
 namespace Chirp.RazorTest;
 
-[Parallelizable(ParallelScope.Self)]
-[TestFixture]
-public class End2EndTest : PageTest
+public class End2EndTest : PlaywrightPageTester
 {
-    private const string PageUrl = "http://localhost:5273/";
-
-    [SetUp]
-    public async Task SetUp()
-    {
-        await Page.GotoAsync(PageUrl);
-    }
-    
     [Test]
     public async Task RegisterAccount()
     {
-        await ClickLink("register");
-
         int id = Random.Shared.Next(1_000_000);
-        await FillInputField("Email", $"testerson{id}@playwright.com");
-        await FillInputField("Username", $"Playwright Testerson {id}");
-        await FillInputField("Password", "Pa$$w0rd", true);
-        await FillInputField("Confirm Password", "Pa$$w0rd");
+        string email = $"testerson{id}@playwright.com";
+        string username = $"Playwright Testerson {id}";
         
-        await ClickButton("Register");
-        
-        await Expect(Page.Locator("button")).ToContainTextAsync($"logout [Playwright Testerson {id}]");
-        await Expect(Page.Locator("h3")).ToContainTextAsync($"What's on your mind Playwright Testerson {id}?");
-        await Expect(Page.Locator("body")).ToContainTextAsync("my timeline");
+        await Register(email, username, "Pa$$w0rd");
+        await AssertLoggedInAs(username);
     }
 
     [Test]
     public async Task Login()
     {
-        await ClickLink("login");
-
-        await FillInputField("Username", "Helge");
-        await FillInputField("password", "LetM31n!");
+        const string username = "Helge";
         
-        await ClickButton("Log in", true);
-        
-        await Expect(Page.Locator("button")).ToContainTextAsync("logout [Helge]");
-        await Expect(Page.Locator("h3")).ToContainTextAsync("What's on your mind Helge?");
-        await Expect(Page.Locator("body")).ToContainTextAsync("my timeline");
+        await Login(username, "LetM31n!");
+        await AssertLoggedInAs(username);
     }
 
     [Test]
     public async Task WriteCheep()
     {
-        await ClickLink("login");
-        
-        await FillInputField("Username", "Helge");
-        await FillInputField("password", "LetM31n!");
-        
-        await ClickButton("Log in", true);
+        const string username = "Helge";
+        await Login(username, "LetM31n!");
         
         int id = Random.Shared.Next(1_000_000);
-        await Page.Locator("#cheepText").ClickAndFill($"Cheeping with playwright! Edition: {id}");
+        string cheep = $"Cheeping with playwright! Edition: {id}";
         
-        await ClickButton("Share");
-        
-        await Expect(Page.Locator("#messagelist")).ToContainTextAsync($"Helge Cheeping with playwright! Edition: {id}");
+        await PostCheep(cheep);
+        await AssertCheepPosted($"{username} {cheep}");
     }
-
-    private async Task ClickLink(string linkName, bool exact = false) => await Page.GetByRole(AriaRole.Link, new() { Name = linkName, Exact = exact }).ClickAsync();
-    private async Task ClickButton(string buttonName, bool exact = false) => await Page.GetByRole(AriaRole.Button, new() { Name = buttonName, Exact = exact }).ClickAsync();
-    private async Task FillInputField(string labelName, string fillText, bool exact = false) => await Page.GetByLabel(labelName, new() { Exact = exact }).ClickAndFill(fillText);
 }
---
+++ test/Chirp.RazorTest/PlaywrightPageTester.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.Playwright;
+
+namespace Chirp.RazorTest;
+
+[Parallelizable(ParallelScope.Self)]
+[TestFixture]
+public abstract class PlaywrightPageTester : PageTest
+{
+    private const string HomepageUrl = "http://localhost:5273/";
+
+    // Locators
+    private ILocator GetLocator(string elementName) => Page.Locator(elementName);
+    private ILocator GetByRole(AriaRole role, string elementName, bool exact) => Page.GetByRole(role, new() { Name = elementName, Exact = exact });
+    private ILocator GetByLabel(string labelName, bool exact) => Page.GetByLabel(labelName, new() { Exact = exact });
+    
+    // Atomic Actions
+    private async Task GotoHomePage() => await Page.GotoAsync(HomepageUrl);
+    private async Task ClickLink(string linkName, bool exact = false) => await GetByRole(AriaRole.Link, linkName, exact).ClickAsync();
+    private async Task ClickButton(string buttonName, bool exact = false) => await GetByRole(AriaRole.Button, buttonName, exact).ClickAsync();
+    private async Task FillInputField(string labelName, string fillText, bool exact = false) => await GetByLabel(labelName, exact).ClickAndFill(fillText);
+    
+    // Composite Actions
+    private protected async Task Register(string email, string username, string password)
+    {
+        await GotoHomePage();
+        await ClickLink("register");
+        
+        await FillInputField("Email", email);
+        await FillInputField("Username", username);
+        await FillInputField("Password", password, true);
+        await FillInputField("Confirm Password", password);
+        
+        await ClickButton("Register");
+    }
+    private protected async Task Login(string username, string password)
+    {
+        await GotoHomePage();
+        await ClickLink("login");
+
+        await FillInputField("Username", username);
+        await FillInputField("password", password);
+        
+        await ClickButton("Log in", true);
+    }
+    private protected async Task PostCheep(string cheep)
+    {
+        await GotoHomePage();
+        await GetLocator("#cheepText").ClickAndFill(cheep);
+        await ClickButton("Share");
+    }
+    
+    // Atomic Assertions
+    private async Task AssertContainsText(string locatorName, string text) => await Expect(GetLocator(locatorName)).ToContainTextAsync(text);
+    
+    // Composite Assertions
+    private protected async Task AssertLoggedInAs(string username)
+    {
+        await GotoHomePage();
+        await AssertContainsText("button", $"logout [{username}]");
+        await AssertContainsText("h3", $"What's on your mind {username}?");
+        await AssertContainsText("body", "my timeline");
+    }
+    private protected async Task AssertCheepPosted(string cheep)
+    {
+        await GotoHomePage();
+        await AssertContainsText("#messagelist", cheep);
+    }
+}
