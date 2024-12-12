cae654fc2ca1fa0471cb00f6ca31cbd9845fb1ed
Niels Bay <nielsarthurbay@gmail.com>
2024-12-12 10:51:37 +0100

    #56 First test done after rebase
    Co-authored-by: Noah <gafa@itu.dk>

---
+++ test/Chirp.RazorTest/UnitTest/UnitTest.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Playwright;
+
+namespace Chirp.RazorTest.UnitTest;
+
+public class UnitTest : PlaywrightPageTester
+{
+    [Test] 
+    public async Task CheepBoxTest(){
+       await Login(DefaultUsername, DefaultPassword);
+       await expect(page.locator('#cheepText')).toBeVisible();
+    }
+}
