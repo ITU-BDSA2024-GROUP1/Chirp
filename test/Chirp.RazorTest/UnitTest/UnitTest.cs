9b1bc3b810b5ea8066b45ef6d88fbd3b9fdb1aa5
Niels Bay <nielsarthurbay@gmail.com>
2024-12-12 11:16:46 +0100

    #56 Made visual uitest for sending cheeps

--- test/Chirp.RazorTest/UnitTest/UnitTest.cs
+++ test/Chirp.RazorTest/UnitTest/UnitTest.cs
@@ -9,4 +9,20 @@ public class UnitTest : PlaywrightPageTester
        await Login(DefaultUsername, DefaultPassword);
        await expect(page.locator('#cheepText')).toBeVisible();
     }
+    [Test]
+    public async Task SendCheepVisualTest(){
+       await Login(DefaultUsername, DefaultPassword);
+       string testCheep = "123";
+       
+       await PostCheep(testCheep);
+       await expect(page.getByRole('button', { name: 'Delete' })).toBeVisible();
+        }
+    [Test]
+    public async Task ToolongCheepTest(){
+       await Login(DefaultUsername, DefaultPassword);
+       string tooLongCheep = "Hello, my baby! Hello, my honey! Hello, my ragtime gal! Send me a kiss by wire. Baby, my heart's on fire! If you refuse me, Honey, you loose me. Then you'll be left alone"
+        string expectedLongCheep = "Hello, my baby! Hello, my honey! Hello, my ragtime gal! Send me a kiss by wire. Baby, my heart's on fire! If you refuse me, Honey, you loose me. Then you'll be"
+       await PostCheep(tooLongCheep);
+       await await AssertCheepPosted(DefaultUsername, expectedLongCheep);
+    }
 }
