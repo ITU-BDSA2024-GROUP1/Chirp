a1fc5d2b25ff00529aa0a7111e3a851a6891e7e2
CptShini <gabrielnoah.fabricius@gmail.com>
2024-12-13 21:35:58 +0100

    Simplified most of the unit tests, but ForgetMe is still not yet done.

--- test/Chirp.RazorTest/UnitTest/UnitTest.cs
+++ test/Chirp.RazorTest/UnitTest/UnitTest.cs
@@ -1,41 +1,44 @@
-﻿using Azure;
-
-using Microsoft.Playwright;
-
-namespace Chirp.RazorTest.UnitTest;
+﻿namespace Chirp.RazorTest.UnitTest;
 
 public class UnitTest : PlaywrightPageTester
 {
     [Test] 
-    public async Task CheepBoxTest(){
+    public async Task CheepBoxVisible()
+    {
        await Login(DefaultUsername, DefaultPassword);
-       await Expect(Page.Locator("#cheepText")).ToBeVisibleAsync();
+       await AssertVisible("#cheepText");
     }
+    
     [Test]
-    public async Task SendCheepVisualTest(){
+    public async Task CheepVisuallyShowsUp()
+    {
        await Login(DefaultUsername, DefaultPassword);
-       string testCheep = "123";
+       
+       const string testCheep = "123";
        
        await PostCheep(testCheep);
-       await AssertContainsText(AriaRole.Button, "Delete");
+       await AssertContainsText("#messagelist", "Delete");
     }
+    
     [Test]
-    public async Task ToolongCheepTest(){
+    public async Task CheepTooLong()
+    {
        await Login(DefaultUsername, DefaultPassword);
-       string tooLongCheep =
-           "Hello, my baby! Hello, my honey! Hello, my ragtime gal! Send me a kiss by wire. Baby, my heart's on fire! If you refuse me, Honey, you loose me. Then you'll be left alone";
-       string expectedLongCheep =
-           "Hello, my baby! Hello, my honey! Hello, my ragtime gal! Send me a kiss by wire. Baby, my heart's on fire! If you refuse me, Honey, you loose me. Then you'll be";
+       
+       const string tooLongCheep = "Hello, my baby! Hello, my honey! Hello, my ragtime gal! Send me a kiss by wire. Baby, my heart's on fire! If you refuse me, Honey, you loose me. Then you'll be left alone";
+       string expectedLongCheep = tooLongCheep[..159];
+       
        await PostCheep(tooLongCheep);
        await AssertCheepPosted(DefaultUsername, expectedLongCheep);
     }
 
     [Test]
-    public async Task ForgetmeTest()
+    public async Task ForgetMe()
     {
         await Register("BazzB@burner1.burner", "BazzB", "Scrh1ft_H");
         await ClickLink("About me");
         await ClickButton("Forget me");
+
+        throw new NotImplementedException("We just need to assert that it worked by attempting to register the account again and having that be successful");
     }
-    
 }
